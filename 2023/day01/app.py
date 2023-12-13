48478c401eefee9e98f7c63a3dc6ea53669d1236
keiche <eiche@DESKTOP-MAH473B.localdomain>
2023-12-12 22:02:50 -0500

    day 1

---
+++ 2023/day01/app.py
@@ -0,0 +1,54 @@
+#!/usr/local/env python3
+"""
+https://adventofcode.com/2023/day/1
+keiche
+"""
+
+import re
+
+part1_total = 0
+part2_total = 0
+
+num_replace = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+with open("1.txt", "r") as f:
+    for line in f.readlines():
+        line = line.rstrip()
+
+        # Part 1
+        nums_only = [x for x in line if x.isdigit()]
+        part1_total += int(nums_only[0] + nums_only[-1])
+
+        # Part 2 - replace words with numbers
+
+        # Array of tuples (index, value)
+        num_indices = []
+
+        # Get index location of the numbers
+        for i, char in enumerate(line):
+            if char.isdigit():
+                num_indices.append((i, int(char)))
+
+        # Get index location of the word numbers
+        for k, v in num_replace.items():
+            for m in re.finditer(k, line):
+                num_indices.append((m.start(0), v))
+
+        num_indices = sorted(num_indices)
+        # Find the lowest and highest index numbers with corresponding value
+        part2_total += int(str(num_indices[0][1]) + str(num_indices[-1][1]))
+
+
+print(f"Part 1: {part1_total}")
+print(f"Part 2: {part2_total}")
